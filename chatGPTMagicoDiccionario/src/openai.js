15e3b341838ebb8a904d09435efc4a80f33e4c7c
Darwin Yusef <wsgestor@gmail.com>
2024-03-19 00:57:56 -0500

    contenidos modificados para incluir en el diccionario magico

--- chatGPTMagicoDiccionario/src/openai.js
+++ chatGPTMagicoDiccionario/src/openai.js
@@ -1,13 +1,68 @@
 const express = require('express');
 const router = express.Router();
-
 const fs = require('fs');
 const OpenAI = require('openai');
+const path = require('path');
+const ElevenLabs = require("elevenlabs-node");
+const apiKeyElevenLabs = process.env.ELEVENLABS_API_KEY;
+
+let voices = new ElevenLabs({ apiKey: apiKeyElevenLabs });
+
+const dates = new Date();
+const dir = dates.getMonth().toString() + dates.getDate().toString();
+function createFolder(dir) {
+    info = fs.mkdir(path.join(__dirname, `./public/audios/${dir.toString()}`),
+        (err) => {
+            if (err.code == 'EEXIST') {
+                console.log('Directory already exist successfully!');
+                return;
+            }
+            if (err) {
+                return console.error(err);
+            }
+            console.log('Directory created successfully!');
+        });
+}
+
+async function activeElevenLabs(ms) {
+    return await voices.textToSpeech({
+        // Required Parameters
+        // The name of your audio file
+        fileName: `./src/public/audios/${dir}/${dates.getTime()}.mp3`,
+        // The text you wish to convert to speech                 
+        textInput: ms,
+        // Optional Parameters
+        // A different Voice ID from the default
+        // The stability for the converted speech
+        voiceId: "ErXwobaYiN019PkySvjV", // Antony
+        // The similarity boost for the converted speech      
+        stability: 0.5,
+        similarityBoost: 0.5,
+        // The ElevenLabs Model ID                        
+        modelId: "eleven_multilingual_v2",
+        // The style exaggeration for the converted speech   
+        style: 1,
+        // The speaker boost for the converted speech                          
+        speakerBoost: true
+    }).then((res) => {
+        return res;
+    }).catch((e) => {
+        console.log(e);
+    });
+}
+
 
 // configuración de OPENAI
 const openai = new OpenAI({
     apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
 });
+
+let modifyJsonStringify = (result) => {
+    if (result) {
+        return JSON.stringify(result.replace(/['"{}]+/g, "").replace(/[:]+/g, ":").replace(/,+/g, ";"))
+    }
+    return '';
+}
 
 async function openAIContent(mensaje) {
     return await openai.chat.completions.create({
@@ -18,34 +73,35 @@ async function openAIContent(mensaje) {
     });
 }
 
-const examples = `
-Ejemplo 1:
-Usuario: Quisiera saber el significado de la palabra "selenofobia".
-Respuesta magica1: La selenofobia es el temor irracional a la Luna. Quienes sufren de selenofobia pueden experimentar ansiedad extrema durante las noches de luna llena, asi como cuando el hombre lobo atemorizaba a miles de personas en la noche y se consumia sus almas."
-Ejemplo 2:
-Usuario: ¿Cuál es el significado de "quimérico"?
-Respuesta magica2: Lo quimérico se refiere a algo ilusorio o fantástico, a menudo relacionado con ideas o proyectos que son difíciles de alcanzar o poco realistas. Es comúnmente utilizado para describir algo que parece tener características de fantasía o imaginación.
-`;
-
 router.get('/chats', async (req, res) => {
     let mensaje = decodeURI(req.query.pregunta);
     if (mensaje === 'undefined') {
         return res.status(404).send('El sitio no ha sido encontrado por favor regrese a la url anterior', 404);
     }
     const data = fs.readFileSync("./src/public/entrenamiento.json", 'utf-8');
+    // console.log(mensaje);
     if (mensaje.toLowerCase().includes('pregunta pregunta')) {
-        mensaje = "Dame una palabra dificil del diccionario que pudiera aprender hoy y su significado y expresalo de manera magica"
+        mensaje = "Dame una palabra magica del diccionario que pudiera aprender hoy y su significado y expresalo de manera magica; que no sea mayor a 20 palabras"
     } else {
-        mensaje = `He llegado a la montaña mas antigua del mundo y he encontrao a un sabio, este sabio tiene un diccionario magico que recibe cualquier 
-        palabra sin importar que tan dificil sea y le da un significado unico magico, yo digo esta palabra ${mensaje} y tu defines el resultado 
-        ten en cuenta estos ejemplos: ${examples}
-        por ultimo ten en cuenta lo siguiente ${JSON.stringify(data)}`
-
+        mensaje = `He llegado a la montaña mas antigua del mundo y he encontrado a un sabio, este sabio tiene un diccionario mágico que recibe cualquier 
+        palabra sin importar que tan difícil sea y le da un consejo ó un significado único y mágico, yo pregunto esto: ( ${mensaje} ) y tu me respondes sabiamente, 
+        tambien ten en cuenta estos ejemplos: 
+      
+        Ejemplo 1:
+        Usuario: Quisiera saber el significado de la palabra "selenofobia".
+        Respuesta magica1: Soy el gran sabio de la montaña con un diccionario mágico en mis manos y te respondo: La selenofobia es el temor irracional a la Luna. Quienes sufren de selenofobia pueden experimentar ansiedad extrema durante las noches de luna llena, asi como cuando el hombre lobo atemorizaba a miles de personas en la noche y se consumía sus almas."
+        Ejemplo 2:
+        Usuario: ¿Cuál es el significado de "quimérico"?
+        Respuesta magica2: Soy el gran sabio de la montaña con un diccionario mágico en mis manos y te respondo Lo quimérico se refiere a algo ilusorio o fantástico, a menudo relacionado con ideas o proyectos que son difíciles de alcanzar o poco realistas. Es comúnmente utilizado para describir algo que parece tener características de fantasía o imaginación.
+        por ultimo ten en cuenta lo siguiente: ${modifyJsonStringify(data)}`
     }
     const chatCompletion = await openAIContent(mensaje);
-
-    // activeElevenLabs(chatCompletion.choices[0].message.content.replace(/\\n/g, "\n"));
-    res.status(201).json({ response: chatCompletion.choices[0].message.content.replace(/\\n/g, "\n") });
+    const info = res;
+    createFolder(dir);
+    activeElevenLabs(chatCompletion.choices[0].message.content.replace(/\\n/g, "\n")).then((res) => {
+        info.status(200).json({ 'response': chatCompletion.choices[0].message.content, res });
+    });
+    /* res.status(200).json({ response: chatCompletion.choices[0].message.content }); */
 });
 
 module.exports = router; 
--- chatGPTMagicoDiccionario/src/public/js/scripts.js
+++ chatGPTMagicoDiccionario/src/public/js/scripts.js
@@ -79,3 +79,105 @@ function fadeIn(el, display) {
     })();
 };
 
+document.querySelector('#buscadorvoz').value = 'pregunta pregunta';
+const audioPlayer = document.getElementById("audio-player");
+const playButton = document.getElementById("play-button");
+const stopButton = document.getElementById("stop-button");
+localStorage.setItem('audiourl', '../audios/principalAudio.mp3');
+
+window.onload = function () {
+    const url = localStorage.getItem('audiourl');
+    console.log(url);
+    audioPlayer.src = `../audios/${url}`; // Cambiar por la URL del primer archivo
+    audioPlayer.play();
+}
+
+document.querySelector('#btnSend').addEventListener('click', (e) => {
+    e.preventDefault();
+    const inputValue = document.querySelector('#buscadorvoz').value;
+    questionCHatGPT(inputValue);
+});
+document.querySelector('#vuelve').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#buscadorvoz').value = 'pregunta pregunta';
+    document.querySelector('#respuesta').textContent = 'Esperando una pregunta a chat gpt...';
+    // document.querySelector('#controlsa').style.display = 'none';
+    location.href = '#page-top';
+});
+
+async function questionCHatGPT(pregunta) {
+    document.querySelector('#spinner').style.display = 'block';
+    document.querySelector('#responseIA').style.display = 'block';
+
+    if (pregunta == '') {
+        pregunta = 'pregunta pregunta';
+    }
+    try {
+        const response = await fetch(`http://localhost:3000/chats?pregunta=${pregunta}`);
+        const question = await response.json();
+        document.querySelector('#spinner').style.display = 'none';
+        document.querySelector('#responseIA').style.display = 'none';
+        console.log(question);
+        document.querySelector('#respuesta').textContent = question.response;
+
+        const sending = `${String(question.res.fileName).replace('./src/public/', '')}`;
+        localStorage.setItem('audiourl', sending);
+        audioPlayer.src = sending;
+        audioPlayer.currentTime = 0;
+        audioPlayer.play();
+
+        location.href = '#about';
+    } catch (error) {
+        console.log(error, "Error en la petición");
+        document.querySelector('#respuesta').textContent = "Error en la api";
+    }
+}
+
+var reconocimiento = new webkitSpeechRecognition();
+reconocimiento.onresult = (event) => {
+    var decirTexto = "";
+    for (var i = event.resultIndex; i < event.results.length; i++) {
+        if (event.results[i].isFinal) {
+            decirTexto = event.results[i][0].transcript;
+        } else {
+            decirTexto += event.results[i][0].transcript;
+        }
+    }
+    document.getElementById('buscadorvoz').value = decirTexto;
+}
+
+const iniciarGrabacion = () => {
+    document.getElementById('buscadorvoz').value = "";
+    document.getElementById('buscadorvoz').focus();
+    reconocimiento.start();
+}
+
+
+
+    ;
+// Función para reproducir el audio al hacer clic en el botón "Reproducir"
+playButton.addEventListener("click", function () {
+    const url = localStorage.getItem('audiourl');
+    audioPlayer.src = `../audios/${url}`;
+    audioPlayer.play();
+});
+
+// Función para detener el audio al hacer clic en el botón "Detener"
+stopButton.addEventListener("click", function () {
+    const url = localStorage.getItem('audiourl');
+    audioPlayer.src = `../audios/${url}`;
+    audioPlayer.pause();
+    // audioPlayer.currentTime = 0; // Reinicia el tiempo de reproducción
+});
+
+// Función para cambiar de audio al finalizar el actual (opcional)
+audioPlayer.addEventListener("ended", function () {
+    const url = localStorage.getItem('audiourl');
+    audioPlayer.src = `../audios/${url}`;
+    audioPlayer.currentTime = 0;
+    audioPlayer.pause();
+});
+
+
+
+
